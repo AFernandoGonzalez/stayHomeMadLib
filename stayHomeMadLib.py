ec862dd3bebc046d304228e6a3a5739ad6b26e56
Fernando Gonzalez <58346389+AFernandoGonzalez@users.noreply.github.com>
2020-12-13 11:23:05 -0500

    initial commit

---
+++ stayHomeMadLib.py
@@ -0,0 +1,20 @@
+
+creature = input('Creature: ')
+adjective = input('Adjective: ')
+verb1 = input('Verb ending in "ED": ')
+feeling = input('Feeling: ')
+clothe = input('Type of clothe: ')
+drink = input('Something sparkly: ')
+superpower1 = input('Superpower: ')
+food1 = input('Favorite food: ')
+game1 = input('Favorite game: ')
+animal = input('Animal: ')
+song = input('Favorite song: ')
+room = input('Room in house: ')
+place = input('Favorite place to go: ')
+password1 = input('Secrete password: ')
+
+
+stayHomeMadLib = f'Once there was a strange time where a {creature} took over the world and made us all stay {adjective} at home. So we {verb1} into our imagination and found out the key to {feeling} was all about wearing {clothe} with {drink}. It turned out that this was also the trick to turning on our {superpower1} and getting to do everything you love all at once. Now, we can eat {food1} while playing {game1}, or search for {animal} while singing {song}. Once time, we even turned {room} into a {place}. Dont belive us? Just say {password1} and well show you.'
+
+print(stayHomeMadLib)
